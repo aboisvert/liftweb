acc700472d12cc2e4864b607fdc30e62984bf637
jorge.ortiz <jorge.ortiz@a1e4567e-5129-0410-9a71-e7582cf7e72e>
2007-11-19 22:10:55 +0000

    Facebook API.
    FacebookRestApi.scala contains a general Facebook API for any Scala application.
    
    Facebook.scala has some lift-specific goodies.
    
    git-svn-id: https://liftweb.googlecode.com/svn/trunk/liftweb@382 a1e4567e-5129-0410-9a71-e7582cf7e72e

---
+++ lift/src/main/scala/net/liftweb/ext_api/facebook/Facebook.scala
@@ -0,0 +1,37 @@
+package net.liftweb.ext_api.facebook;
+
+import net.liftweb.http.{S, SessionVar}
+import net.liftweb.util.Helpers._
+import net.liftweb.util.{Can, Empty, Failure, Full}
+
+object Facebook {
+  object API extends SessionVar[Can[FacebookClient]](sessionKey.map(k => new FacebookClient(k)))
+  
+  def canvasPage_? : boolean = S.param("fb_sig_in_canvas") match {
+    case Full(num) if toInt(num) == 1 => true
+    case _ => false
+  }
+  
+  def addedApplication_? : boolean = S.param("fb_sig_added") match {
+    case Full(num) if toInt(num) == 1 => true
+    case _ => false
+  }
+  
+  def loggedIn_? : boolean = S.param("fb_sig_user") match {
+    case Full(num) if toInt(num) > 0 => true
+    case _ => false
+  }
+  
+  def userId: Can[Int] = S.param("fb_sig_user") match {
+    case Full(num) => Full(toInt(num))
+    case _ => Empty
+  }
+  
+  def userId_! : Int = userId.open_!
+  
+  def sessionKey : Can[String] = S.param("fb_sig_session_key")
+  
+  def sessionKey_! : String = sessionKey.open_!
+  
+  def loginUrl: String = "http://www.facebook.com/login.php?api_key=" + FacebookRestApi.apiKey + "&v=1.0"
+}
