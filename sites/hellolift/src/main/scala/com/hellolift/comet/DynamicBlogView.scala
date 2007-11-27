8eba62991db7c5a2beec7150d2fab07de2b821d0
stevej <stevej@a1e4567e-5129-0410-9a71-e7582cf7e72e>
2007-11-27 23:18:01 +0000

    Checking in the Dynamic Blog View comet actor for the blogging example.
    git-svn-id: https://liftweb.googlecode.com/svn/trunk/liftweb@400 a1e4567e-5129-0410-9a71-e7582cf7e72e

---
+++ sites/hellolift/src/main/scala/com/hellolift/comet/DynamicBlogView.scala
@@ -0,0 +1,51 @@
+package com.hellolift.comet
+
+import net.liftweb.http._
+import net.liftweb.util._
+import net.liftweb.sitemap._
+import net.liftweb.sitemap.Loc._
+import scala.xml._
+
+import com.hellolift.model.Entry
+import com.hellolift.controller.BlogCache
+import com.hellolift.controller.BlogUpdate
+import com.hellolift.controller.AddBlogWatcher
+
+class DynamicBlogView(theSession: LiftSession, name: Can[String], defaultXml: NodeSeq, attributes: Map[String, String]) extends 
+CometActor(theSession, name, defaultXml, attributes) {
+  def defaultPrefix = "blog"
+  var blogtitle = ""
+  var blog : List[Entry] = Nil
+  var blogid : long = 0L
+
+  def _entryview(e : Entry) : Node = {
+    <div>
+    <strong>{e.title}</strong><br />
+    <span>{e.body}</span>
+    </div>
+  }
+
+  // render draws the content on the screen.
+  def render = {
+    bind("view" -> <span>{blog.flatMap(e => _entryview(e))}</span>)
+  }
+
+  // localSetup is the first thing run, we use it to setup the blogid or 
+  // redirect them to / if no blogid was given.
+  override def localSetup {
+    name match {
+      case Full(t) => this.blogid = Helpers.toLong(t)
+      case _ => S.redirectTo("/") // if they don't request a blog, send them away.
+    }
+
+    // Let the BlogCache know that we are watching for updates for this blog.
+    (BlogCache.cache !? AddBlogWatcher(this, this.blogid)) match {
+      case BlogUpdate(entries) => this.blog = entries 
+    }
+  }
+  
+  // lowPriority will receive messages sent from the BlogCache
+  override def lowPriority : PartialFunction[Any, Unit] = {
+    case BlogUpdate(entries : List[Entry]) => this.blog = entries; reRender(false)
+  }
+}
