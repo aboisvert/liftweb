329c7ed307eed10565328bac5edf48dde2ad9684
david.bernard.31 <david.bernard.31@a1e4567e-5129-0410-9a71-e7582cf7e72e>
2007-12-12 12:19:20 +0000

    restore version of maven-scala-plugin to empty (2.2 released)
    git-svn-id: https://liftweb.googlecode.com/svn/trunk/liftweb@492 a1e4567e-5129-0410-9a71-e7582cf7e72e

--- lift-archetype-basic/src/main/resources/archetype-resources/src/test/scala/LiftConsole.scala
+++ lift-archetype-basic/src/main/resources/archetype-resources/src/test/scala/LiftConsole.scala
@@ -2,14 +2,14 @@ import bootstrap.liftweb.Boot
 import scala.tools.nsc.MainGenericRunner
 
 object LiftConsole {
-def main(args : Array[String]) {
-// Instantiate your project's Boot file
-val b = new Boot();
-// Boot your project
-b.boot;
-// Now run the MainGenericRunner to get your repl
-MainGenericRunner.main(args)
-// After the repl exits, then exit the scala script
-exit(0)
-}
+  def main(args : Array[String]) {
+    // Instantiate your project's Boot file
+    val b = new Boot();
+    // Boot your project
+    b.boot;
+    // Now run the MainGenericRunner to get your repl
+    MainGenericRunner.main(args)
+    // After the repl exits, then exit the scala script
+    exit(0)
+  }
 }
