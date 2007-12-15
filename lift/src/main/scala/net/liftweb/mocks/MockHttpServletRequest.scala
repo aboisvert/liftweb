77b918b53b7564ad5476adc854ac345dcccd8737
stevej <stevej@a1e4567e-5129-0410-9a71-e7582cf7e72e>
2007-12-15 01:51:15 +0000

    A group of Mock Objects used to simulate an HTTP request/response cycle. Look at the top of MockServletContext.scala for an example test
    git-svn-id: https://liftweb.googlecode.com/svn/trunk/liftweb@502 a1e4567e-5129-0410-9a71-e7582cf7e72e

---
+++ lift/src/main/scala/net/liftweb/mocks/MockHttpServletRequest.scala
@@ -0,0 +1,133 @@
+package net.liftweb.mocks
+
+import scala.collection.mutable.HashMap
+import java.io.PrintWriter
+import java.io.StringReader
+import java.io.BufferedReader
+import java.io.ByteArrayOutputStream
+import java.io.ByteArrayInputStream
+import java.io.FileInputStream
+import java.io.InputStream
+import java.io.StringBufferInputStream
+import java.io.File
+import java.util.Arrays
+import java.util.Date
+import java.util.Locale
+import java.util.Vector
+import javax.servlet._
+import javax.servlet.http._
+import net.liftweb.util.Helpers
+
+/**
+ * A Mock ServletRequest. Change it's state to to create the request you are 
+ * interested in. At the very least, you will need to change method and path.
+ *
+ * @author Steve Jenson (stevej@pobox.com)
+ */
+class MockHttpServletRequest extends HttpServletRequest {
+  var session : HttpSession = new MockHttpSession
+  var queryString: String = ""
+  var contextPath = ""
+  var path = ""
+  var method = "GET"
+  val headers: scala.collection.jcl.HashMap[String, String] = 
+    new scala.collection.jcl.HashMap[String, String](new java.util.HashMap)
+  val attr: scala.collection.jcl.HashMap[String, Any] = 
+    new scala.collection.jcl.HashMap[String, Any](new java.util.HashMap)
+  var cookies: List[Cookie] = Nil
+  var authType = null 
+  var localPort = 0
+  var localAddr = null 
+  var localName = null 
+  var remotePort = 0
+  var remoteHost = null
+  var remoteAddr = null
+  var locale = Locale.getDefault
+  var reader: BufferedReader = new BufferedReader(new StringReader(method + " " + path +  "/\r\n\r\n"))
+  var serverPort = 0
+  var serverName = null
+  var scheme = "http"
+  var protocol = "http 1.0"
+  var parameterMap: scala.collection.jcl.HashMap[String, String] = 
+    new scala.collection.jcl.HashMap[String, String](new java.util.HashMap)
+  val sbis = new StringBufferInputStream("")
+  var inputStream: ServletInputStream = new MockServletInputStream(sbis)
+  var contentType = null
+  var contentLength = 0
+  var charEncoding = "ISO-8859-1" // HTTP's default encoding
+
+  def isRequestedSessionIdFromURL = false
+  def isRequestedSessionIdFromUrl = false
+  def isRequestedSessionIdFromCookie = false
+  def isRequestedSessionIdValid = false
+  def getSession(p: Boolean) = {
+    session
+  }
+  def getSession = getSession(false)
+  def getServletPath = ""
+  def getRequestURL = new StringBuffer(path)
+  def getRequestURI = path
+  def getRequestedSessionId = null
+  def getUserPrincipal = null
+  def isUserInRole(user: String): Boolean = false
+  def getRemoteUser = ""
+  def getQueryString = queryString
+  def getContextPath = contextPath
+  def getPathTranslated = path
+  def getPathInfo = path
+  def getMethod = method
+  def getIntHeader(h: String): Int = {
+    Helpers.toInt(headers(h))
+  }
+  def getHeaderNames = {
+    new Vector(headers.underlying.keySet).elements
+  }
+  def getHeaders = headers
+  def getHeaders(s: String) = {
+    val v = new Vector()
+    v.add(headers(s))
+    v.elements
+  }
+  def getHeader(h: String) = headers.get(h) match {
+    case Some(v) => v
+    case None => null
+  }
+  def getDateHeader(h: String): Long = {
+    Helpers.toLong(headers(h))
+  }
+  def setDateHeader(s: String, l: Long) {
+    headers += s -> l.toString
+  }
+  def getCookies = cookies.toArray
+  def getAuthType = authType
+  def getLocalPort = localPort
+  def getLocalAddr = localAddr
+  def getLocalName = localName
+  def getRemotePort = remotePort
+  def getRealPath(s: String) = s
+  def getRequestDispatcher(s: String): RequestDispatcher = null
+  def isSecure = false
+  def getLocales = new Vector(Arrays.asList(Locale.getAvailableLocales.asInstanceOf[Array[Object]])).elements
+  def getLocale = locale
+  def removeAttribute(key: String) = attr -= key
+  def setAttribute(key: String, value: Any) = attr += key -> value
+  def getRemoteHost = remoteHost
+  def getRemoteAddr = remoteAddr
+  def getReader = reader
+  def getServerPort = serverPort
+  def getServerName = serverName
+  def getScheme = scheme
+  def getProtocol = protocol
+  def getParameterMap = parameterMap.underlying
+  def getParameterValues(key: String) = 
+    parameterMap.underlying.values.toArray.asInstanceOf[Array[String]]
+  def getParameterNames = new Vector(parameterMap.underlying.keySet).elements
+  def getParameter(key: String) = parameterMap(key)
+  def getInputStream = inputStream
+  def getContentType = contentType
+  def getContentLength = contentLength
+  def getCharacterEncoding = charEncoding
+  def setCharacterEncoding(enc: String) = charEncoding = enc
+  def getAttributeNames = new Vector(attr.underlying.keySet).elements
+  def getAttribute(key: String) = attr(key).asInstanceOf[Object]
+}
---
+++ lift/src/main/scala/net/liftweb/mocks/MockHttpServletResponse.scala
@@ -0,0 +1,122 @@
+package net.liftweb.mocks
+
+import scala.collection.mutable.HashMap
+import java.io.PrintWriter
+import java.io.StringReader
+import java.io.BufferedReader
+import java.io.ByteArrayOutputStream
+import java.io.ByteArrayInputStream
+import java.io.FileInputStream
+import java.io.InputStream
+import java.io.StringBufferInputStream
+import java.io.File
+import java.util.Arrays
+import java.util.Date
+import java.util.Locale
+import java.util.Vector
+import javax.servlet._
+import javax.servlet.http._
+
+/** 
+ * A Mock HttpServletResponse. Take a peek at it's writer or
+ * outputStream to see what lift has written in response to your request
+ * 
+ * @param writer a PrintWriter that the response will be written with
+ * @param outputStream an OutputStream that the response will be written to.
+ * 
+ * @author Steve Jenson (stevej@pobox.com)
+ */
+class MockHttpServletResponse(var writer: PrintWriter, var outputStream: ServletOutputStream) 
+  extends HttpServletResponse {
+  var statusCode : Int = 200
+  var statusString : String = "OK"
+  var contentType = "text/html"
+  var contentLength = 0
+  val headers: HashMap[String, String] = new HashMap[String, String]
+  var cookies: List[Cookie] = Nil
+  var locale: Locale = Locale.getDefault
+  var bufferSize: Int = 0
+  var charEncoding = "ISO-8859-1" // yes, that's HTTP's default
+
+  def setStatus(i: Int, s: String) = {
+    statusCode = i
+    statusString = s
+  }
+
+  def setStatus(i: Int) = {
+    statusCode = i
+  }
+
+  def addIntHeader(s: String, i: Int) {
+    headers += s -> i.toString
+  }
+  def setIntHeader(s: String, i: Int) {
+    headers += s -> i.toString
+  }
+  def addHeader(s1: String, s2: String) {
+    headers += s1 -> s2
+  }
+  def setHeader(s1: String, s2: String) {
+    headers += s1 -> s2
+  }
+
+  def addDateHeader(s: String, l: Long) {
+    headers += s -> (new Date(l)).toString
+  }
+  def setDateHeader(s: String, l: Long) {
+    addDateHeader(s, l)
+  }
+
+  def sendRedirect(uri: String) {
+    // Send back a 301 to the URL mentioned
+    statusCode = 301
+    addHeader("Location", uri)
+  }
+
+  def sendError(code: Int) {
+    statusCode = code    
+  }
+
+  def sendError(code: Int, s: String) {
+    sendError(code)
+    statusString = s
+  }
+
+  def encodeRedirectURL(url: String): String = encodeRedirectUrl(url)
+  def encodeRedirectUrl(url: String): String = {
+    // do something fancy encoding on uri, return that.
+    url
+  }
+  def encodeURL(url: String): String = encodeUrl(url)
+  def encodeUrl(url: String): String = {
+    // use the same encoder as encodeRedirectUrl
+    url
+  }
+  def containsHeader(header: String): Boolean = {
+    headers.contains(header)
+  }
+  def addCookie(cookie: Cookie) = {
+    cookies =  cookie :: cookies
+  }
+  def getLocale: Locale = locale
+  def setLocale(l: Locale) = locale = l
+  def reset {
+    // well, reset all the state to it's original values. yikes. later.
+  }
+  def isCommitted = false
+  def resetBuffer { 
+    // reset the buffer. 
+  }
+  def flushBuffer {
+    // flush the buffer
+  }
+  def getBufferSize = bufferSize
+  def setBufferSize(i: Int) = bufferSize = i
+  def setContentType(t: String) = contentType = t
+  def setContentLength(l: Int) = contentLength = l
+  def setCharacterEncoding(e: String) = charEncoding = e
+  def getWriter = writer
+  def getOutputStream = outputStream
+  def getContentType = contentType
+  def getCharacterEncoding = charEncoding
+}
