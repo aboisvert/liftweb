dc7f3bde32d86a26fe9c6d4a6842ee755608fc9b
david.bernard.31 <david.bernard.31@a1e4567e-5129-0410-9a71-e7582cf7e72e>
2007-12-30 16:01:31 +0000

    clean sample code
    git-svn-id: https://liftweb.googlecode.com/svn/trunk/liftweb@524 a1e4567e-5129-0410-9a71-e7582cf7e72e

--- lift-archetype-basic/src/main/resources/archetype-resources/src/test/scala/RunWebApp.scala
+++ lift-archetype-basic/src/main/resources/archetype-resources/src/test/scala/RunWebApp.scala
@@ -1,33 +1,28 @@
 import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Server;
-import org.mortbay.jetty.bio.SocketConnector;
 import org.mortbay.jetty.webapp.WebAppContext;
 
 object RunWebApp extends Application {
-        val server = new Server();
-        val connector = new SocketConnector();
-        connector.setPort(8888);
-        server.setConnectors(Array(connector));
+  val server = new Server(8080);
+  val context = new WebAppContext()
+  context.setServer(server)
+  context.setContextPath("/")
+  context.setWar("src/main/webapp")
 
-        val bb = new WebAppContext()
-        bb.setServer(server)
-        bb.setContextPath("/")
-        bb.setWar("src/main/webapp")
+  server.addHandler(context)
 
-        server.addHandler(bb)
-
-        try {
-            println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP");
-            server.start();
-            while (System.in.available() == 0) {
-                Thread.sleep(5000)
-            }
-            server.stop()
-            server.join()
-        } catch {
-          case exc : Exception => {
-            exc.printStackTrace()
-            System.exit(100)
-          }
-        }
+  try {
+    println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP");
+    server.start();
+    while (System.in.available() == 0) {
+      Thread.sleep(5000)
+    }
+    server.stop()
+    server.join()
+  } catch {
+    case exc : Exception => {
+      exc.printStackTrace()
+      System.exit(100)
+    }
+  }
 }
