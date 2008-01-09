b209b2e57840efcd72925d5c0699bdf1c12e37e6
etorreborre <etorreborre@a1e4567e-5129-0410-9a71-e7582cf7e72e>
2008-01-09 11:02:11 +0000

    modified the Can unit tests to use scalacheck only
    git-svn-id: https://liftweb.googlecode.com/svn/trunk/liftweb@569 a1e4567e-5129-0410-9a71-e7582cf7e72e

--- lift/src/test/scala/net/liftweb/util/CanUnit.scala
+++ lift/src/test/scala/net/liftweb/util/CanUnit.scala
@@ -3,26 +3,25 @@ import org.specs._
 import net.liftweb.util.Can._
 import org.specs.runner._
 import org.specs.Sugar._
-import org.specs.util.DataTables
 import org.scalacheck.Gen._
 import org.specs.matcher.ScalacheckParameters._
+import org.scalacheck._
+import Arbitrary._
+import Gen._
+import Prop._
 
 class CanUnitTest extends JUnit3(CanUnit)
 object CanUnitRunner extends ConsoleRunner(CanUnit)
-object CanUnit extends Specification with DataTables with CanValues {
+object CanUnit extends Specification with CanGen {
   "A Can equals method" should {
-    "return true with comparing 2 Full having the same value" in {
-      Full(1) must_== Full(1)
-    }
-    "return true with comparing 1 Full and its value" in {
-      Full(1) must_== 1
-    }
-    "return true with comparing 2 Empty" in {
-      Empty must_== Empty
-    }
-    "return false with comparing one Full and one Empty" in {
-      Full(1) must_!= Empty
-      Empty must_!= Full(1)
+    "return true with comparing two identical Can messages" in {
+      val equality = (c1: Can[Int], c2: Can[Int]) => (c1, c2) match {
+        case (Empty, Empty) => c1 == c2
+        case (Full(x), Full(y)) => (c1 == c2) == (x == y)
+        case (Failure(m1, e1, l1), Failure(m2, e2, l2)) => (c1 == c2) == ((m1, e1, l1) == (m2, e2, l2))
+        case _ => c1 != c2
+      }
+      property(equality) must pass
     }
     "return false with comparing one Full and another object" in {
       Full(1) must_!= "hello"
@@ -30,36 +29,32 @@ object CanUnit extends Specification with DataTables with CanValues {
     "return false with comparing one Empty and another object" in {
       Empty must_!= "hello"
     }
-    "return true with comparing two identical Failure messages" in {
-      "message" | "exception" | "chain"    |
-      "msg"     ! empty       !  nil       | 
-      "msg"     ! full        !  chainOf2  | 
-      "msg"     ! full        !  nil       | 
-      "msg"     ! full        !  chainOf1  |> {(msg: String, exception: Can[Throwable], chain: List[Failure]) =>
-         Failure(msg, exception, chain) must_== Failure(msg, exception, chain) 
-      }
-    }
-    "return false with comparing two different Failure messages" in {
-      val failureData = for (m1 <- elements("msg", "msg2");
-                             m2 <- elements("msg", "msg2") if (m1 != m2);
-                             e1 <- elements(empty, full);
-                             e2 <- elements(empty, full) if (e1 != e2);
-                             c1 <- elements(nil, chainOf1, chainOf2);
-                             c2 <- elements(nil, chainOf1, chainOf2) if (c1 != c2))
-                             yield (m1, e1, c1, m2, e2, c2)
-
-      failureData must pass { t: (String, Can[Throwable], List[Failure], String, Can[Throwable], List[Failure]) => 
-        val (msg, exception, chain, otherMsg, otherEx, otherChain) = t
-        Failure(msg, exception, chain) must_!= Failure(otherMsg, otherEx, otherChain) 
-      }(set(minTestsOk -> 50))
+    "return false with comparing one Failure and another object" in {
+      Failure("", Empty, Nil) must_!= "hello"
     }
   }
 }
-trait CanValues {
-  case class LiftException extends Exception
-  val empty: Can[LiftException] = Empty
-  val full: Can[LiftException] = Full(LiftException())
-  val nil: List[Failure] = Nil
-  val chainOf2 = List(Failure("e", Full(LiftException()), Nil))
-  val chainOf1 = List(Failure("e", Empty, Nil))
+trait CanGen {
+  
+  implicit def genThrowable(dummy: Arb[Throwable]): Arbitrary[Throwable] = new Arbitrary[Throwable] {
+    case class UserException extends Throwable
+    def getArbitrary = value(UserException())
+  }
+
+  implicit def genCan[T](dummy: Arb[Can[T]])(implicit a: Arb[T] => Arbitrary[T]): Arbitrary[Can[T]] = new Arbitrary[Can[T]] {
+    def getArbitrary = frequency(
+      (3, value(Empty)),
+      (3, arbitrary[T].map(Full[T])),
+      (1, genFailureCan)
+    )
+  }
+
+  def genFailureCan: Gen[Failure] = for {
+    msgLen <- choose(0, 4)
+    msg <- vectorOf(msgLen, alphaChar)
+    exception <- arbitrary[Can[Throwable]]
+    chainLen <- choose(1, 5)
+    chain <- frequency((1, vectorOf(chainLen, genFailureCan)), (3, value(Nil)))
+  } yield Failure(msg.mkString, exception, chain.toList) 
+ 
 }
