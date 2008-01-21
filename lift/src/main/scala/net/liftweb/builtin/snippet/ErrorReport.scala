14bb4b3fb27a752095909d2e72bf1f71c6ec3c63
feeder.of.the.bears <feeder.of.the.bears@a1e4567e-5129-0410-9a71-e7582cf7e72e>
2008-01-21 15:19:11 +0000

    Error and notice messages can have the classes defined in a class attribute
    git-svn-id: https://liftweb.googlecode.com/svn/trunk/liftweb@614 a1e4567e-5129-0410-9a71-e7582cf7e72e

--- lift/src/main/scala/net/liftweb/builtin/snippet/ErrorReport.scala
+++ lift/src/main/scala/net/liftweb/builtin/snippet/ErrorReport.scala
@@ -13,16 +13,16 @@ import net.liftweb.util.Helpers._
 
 class ErrorReport {
   def render(styles: Group): NodeSeq =
-    List((S.errors, (styles \\ "error_msg"), "Error", (styles \\ "error_class")),
-        (S.warnings, (styles \\ "warning_msg"), "Warning", (styles \\ "warning_class")),
-        (S.notices, (styles \\ "notice_msg"), "Notice", (styles \\ "notice_class"))).flatMap {
+    List((S.errors, (styles \\ "error_msg"), "Error", ((styles \\ "error_class") ++ (styles \\ "error_msg" \\ "@class"))),
+        (S.warnings, (styles \\ "warning_msg"), "Warning", ((styles \\ "warning_class")++ (styles \\ "error_msg" \\ "@class"))),
+        (S.notices, (styles \\ "notice_msg"), "Notice", ((styles \\ "notice_class")) ++ (styles \\ "notice_msg" \\ "@class"))).flatMap {
     case (msg, titleList, defaultTitle, styleList) =>
       val title: String = titleList.toList.filter(_.prefix == "lift").map(_.text.trim).filter(_.length > 0) headOr defaultTitle
       
       msg.toList.map(e => (<li>{e}</li>) ) match {
         case Nil => Nil
         case msgList => val ret = (<div>{title}:<ul>{msgList}</ul></div>)
-        styleList.toList.filter(_.prefix == "lift").map(_.text.trim).foldLeft(ret)((xml, style) => xml % new UnprefixedAttribute("class", style, Null))
+        styleList.toList.map(_.text.trim).foldLeft(ret)((xml, style) => xml % new UnprefixedAttribute("class", style, Null))
       }
     }
 }
