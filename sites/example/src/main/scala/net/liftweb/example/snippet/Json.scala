104b5a93ad626cf78ce03accf240c0dfba8dbbce
stevej <stevej@a1e4567e-5129-0410-9a71-e7582cf7e72e>
2008-02-19 17:16:36 +0000

    more whitespace changes.
    git-svn-id: https://liftweb.googlecode.com/svn/trunk/liftweb@728 a1e4567e-5129-0410-9a71-e7582cf7e72e

--- sites/example/src/main/scala/net/liftweb/example/snippet/Json.scala
+++ sites/example/src/main/scala/net/liftweb/example/snippet/Json.scala
@@ -29,11 +29,11 @@ class Json {
   object json extends JsonHandler {
     def apply(in: Any): JsCmd =
       SetHtml("json_result", in match {
-  case JsonCmd("show", _, p: String, _) => Text(p)
-  case JsonCmd("textile", _, p: String, _) =>
-    TextileParser.toHtml(p, Empty)
-  case JsonCmd("count", _, p: String, _) => Text(p.length+" Characters")
-  case x => <b>Problem... didn't handle JSON message {x}</b>
+	case JsonCmd("show", _, p: String, _) => Text(p)
+	case JsonCmd("textile", _, p: String, _) =>
+	  TextileParser.toHtml(p, Empty)
+	case JsonCmd("count", _, p: String, _) => Text(p.length+" Characters")
+	case x => <b>Problem... didn't handle JSON message {x}</b>
       })
   }
 
