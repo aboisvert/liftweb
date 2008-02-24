f8f2ca6811f955f7112d1bfb02bec1ccf58a185e
etorreborre <etorreborre@a1e4567e-5129-0410-9a71-e7582cf7e72e>
2008-02-24 11:56:13 +0000

    deleted the erroneous branch for SCALA_2_7_0
    git-svn-id: https://liftweb.googlecode.com/svn/trunk/liftweb@754 a1e4567e-5129-0410-9a71-e7582cf7e72e

--- lift/SCALA_2_7_0/src/main/scala/net/liftweb/http/RedirectException.scala
+++
@@ -1,9 +0,0 @@
-package net.liftweb.http
-
-/*                                                *\
-  (c) 2007 WorldWide Conferencing, LLC
-  Distributed under an Apache License
-  http://www.apache.org/licenses/LICENSE-2.0
-\*                                                 */
-
-class RedirectException(val msg : String,val to : String) extends Exception(msg)
