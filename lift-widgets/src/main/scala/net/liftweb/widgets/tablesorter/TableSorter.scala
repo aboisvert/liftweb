85d881cec31e7df4e130b84411e4b56cd4e999e6
david.bernard.31 <david.bernard.31@a1e4567e-5129-0410-9a71-e7582cf7e72e>
2008-04-24 21:19:19 +0000

    add configuration to run a test webapp (from src/test) with jetty add a first/basic version for TableSorter widget
    git-svn-id: https://liftweb.googlecode.com/svn/trunk/liftweb@949 a1e4567e-5129-0410-9a71-e7582cf7e72e

---
+++ lift-widgets/src/main/scala/net/liftweb/widgets/tablesorter/TableSorter.scala
@@ -0,0 +1,31 @@
+package net.liftweb.widgets.tablesorter
+
+import scala.xml.NodeSeq
+
+class TableSorter {
+  def init() {
+    import net.liftweb.http.ResourceServer
+
+    ResourceServer.allow({
+      case "tablesorter" :: tail => true
+    })
+    println("register")
+  }
+  def render(selector: String) = {
+    val onLoad ="""jQuery(function($){
+            // tablesorter call - maybe here?
+            $('"""+selector+"""').tablesorter({sortList:[[0,0]], widgets:['zebra']});
+            });
+            """
+    <xml:group>
+      <head>
+        <link rel="stylesheet" href="/classpath/tablesorter/themes/blue/style.css" type="text/css" id="" media="print, projection, screen" />
+        <script type="text/javascript" src="/classpath/tablesorter/jquery.tablesorter.js"></script>
+        <script type="text/javascript" charset="utf-8">{onLoad}</script>
+      </head>
+      <table>
+      <tr><td>33</td></tr>
+      </table>
+    </xml:group>
+  }
+}
