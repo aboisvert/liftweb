89a08254c9c111531dfbaef10b32603496f2701d
David Pollak <feeder.of.the.bears@gmail.com>
2008-07-13 19:41:58 -0700

    Added a helper to Combinator stuff

--- lift/src/main/scala/net/liftweb/util/CombParserHelpers.scala
+++ lift/src/main/scala/net/liftweb/util/CombParserHelpers.scala
@@ -113,6 +113,8 @@ trait CombParserHelpers {
 
   def notEOL: Parser[Elem] = (not(EOL) ~> anyChar)
 
+    def notEOF: Parser[Elem] = (not(accept('\032')) ~> anyChar)
+
   def anyChar: Parser[Elem] = elem("Any Char", c => c != '\032')
 
   /**
