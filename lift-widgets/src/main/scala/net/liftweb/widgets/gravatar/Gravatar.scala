e318278327d068238a72ad77fa1d33f88f80a1d6
Timothy Perrett <timperrett@Timothy-Perrett.local>
2008-11-01 11:14:47 +0000

    Redesigned the Gravatar widget

--- lift-widgets/src/main/scala/net/liftweb/widgets/gravatar/Gravatar.scala
+++ lift-widgets/src/main/scala/net/liftweb/widgets/gravatar/Gravatar.scala
@@ -1,34 +1,64 @@
 package net.liftweb.widgets.gravatar
 
-import _root_.scala.xml.{NodeSeq, Text, Group, Node, Elem}
-import _root_.net.liftweb.http._
-import _root_.net.liftweb.http.S
-import _root_.net.liftweb.mapper._
-import _root_.net.liftweb.http.S._
-import _root_.net.liftweb.util.Helpers._
-import _root_.net.liftweb.util._
-import _root_.java.util.Locale
+import _root_.scala.xml.NodeSeq
 import _root_.java.security._
+import _root_.java.util._
+import _root_.java.io._
+import _root_.net.liftweb.util.Log
 
-// gravatar_id - MD5 sum of your email address
-// size - image size
-// rating - rating of the image, let's start with "G" which is also the default
 
-class Gravatar {
-  private def getMD5(s: String): String = {
-    val m = MessageDigest.getInstance("MD5")
-    m.update(s.getBytes(),0,s.length())
-    BigInt(1,m.digest()).toString(16)
+/**
+ * To make a Gravatar: 
+ *
+ * <pre><code>
+ * Gravatar("email@domain.com") // => Produces a gravatar thats 42x42 with a G rating
+ * Gravatar("email@domain.com", 50) // => Produces a gravatar thats 50x50 with a G rating
+ * Gravatar("email@domain.com", 50, "R") // => Produces a gravatar thats 50x50 with a R rating
+ * </code></pre>
+ *
+ */
+object Gravatar {
+  
+  val defaultSize: Int = 42
+  val defaultRating: String = "G"
+  val avatarEndpoint: String = "http://www.gravatar.com/avatar/"
+  
+  /**
+   * @param e The email address of the recipient
+   */
+  def apply(e: String): NodeSeq = url(e,defaultSize,defaultRating)
+
+  /**
+   * @param e The email address of the recipient
+   * @param s The square size of the output gravatar
+   */
+  def apply(e: String, s: Int): NodeSeq = url(e,s,defaultRating)
+  
+  /**
+   * @param e The email address of the recipient
+   * @param s The square size of the output gravatar
+   * @param r The rating of the Gravater, the default is G
+   */
+  def apply(e: String, s: Int, r: String) = url(e,s,r)
+  
+  private def url(email: String, size: Int, rating: String): NodeSeq = {
+    html(avatarEndpoint + getMD5(email) + "?s=" + size.toString + "&r=" + rating)
   }
-
-  def render(e: String): NodeSeq = {
-    var src = "http://www.gravatar.com/avatar.php?gravatar_id=" + getMD5(e)
-    src = src + "&size=" + S.attr("s").openOr("42")
-    src = src + "&rating=" + S.attr("r").openOr("G")
-
-    <div id="gravatar_wrapper"><div id="gravatar_image"><img src={src}></img></div></div>
+  
+  private def html(in: String): NodeSeq = {
+    <div id="gravatar_wrapper"><div id="gravatar_image"><img src={in} alt="Gravater" /></div></div>
+  }
+  
+  private def getMD5(message: String): String = {
+    val md: MessageDigest = MessageDigest.getInstance("MD5")
+    val bytes = message.getBytes("CP1252")
+    
+    try {
+      BigInt(1,md.digest(bytes)).toString(16)
+    } catch {
+      case a: NoSuchAlgorithmException => Log.error("[Gravater] No Algorithm.", a); ""
+      case x: UnsupportedEncodingException => Log.error("[Gravater] Unsupported Encoding.", x); ""
+      case _ => Log.error("[Gravater] Unknown error."); ""
+    }
   }
 }
-
-
-
