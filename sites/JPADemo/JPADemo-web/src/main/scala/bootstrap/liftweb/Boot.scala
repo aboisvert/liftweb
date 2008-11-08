fcd44adf480f5788406c6adb5e8a0484871d7650
Derek Chen-Becker <github@chen-becker.org>
2008-11-08 09:01:22 -0700

    Fixed copyright and package names for JPA demo site.

--- sites/JPADemo/JPADemo-web/src/main/scala/bootstrap/liftweb/Boot.scala
+++ sites/JPADemo/JPADemo-web/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2008 WorldWide Conferencing, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
 package bootstrap.liftweb
 
 import java.util.Locale
@@ -8,7 +23,7 @@ import net.liftweb.util.{Can,Empty,Full,LoanWrapper,LogBoot}
 import net.liftweb.http._
 import net.liftweb.sitemap._
 import net.liftweb.sitemap.Loc._
-import com.foo.jpaweb.model._
+import net.liftweb.jpademo.model._
 import S.?
 
 /**
@@ -32,7 +47,7 @@ class Boot {
       """
 
     // where to search snippet
-    LiftRules.addToPackages("com.foo.jpaweb")
+    LiftRules.addToPackages("net.liftweb.jpademo")
 
     // Set up a site map
     val entries = SiteMap(Menu(Loc("Home", "index" :: Nil , ?("Home"))),
