5332c1f188e077df3d5d3dcd390aac61aa1b89ca
danciu <danciu@alimar.(none)>
2008-12-23 22:47:31 +0200

    Removed commented code

--- lift-util/src/main/scala/net/liftweb/util/HeadHelper.scala
+++ lift-util/src/main/scala/net/liftweb/util/HeadHelper.scala
@@ -70,58 +70,6 @@ object HeadHelper {
     }
   }
 
-  /*
-   val headChildren = new NodeBuffer();
-   def extractHead2(nodes: Iterable[Node]): NodeSeq = {
-   val zs = new NodeBuffer();
-   for(val z <- nodes) { zs &+ extractHead(z) }
-   zs
-   }
-   def extractHead(node: NodeSeq):NodeSeq = node match {
-   case Elem(namespace, "html", attrs, scp, children @ _*) => {
-   val newChildren = extractHead2(children)
-   val newHead = cleanHead(<head>{headChildren}</head>)
-   Elem(namespace, "html", attrs, scp, (newHead ++ newChildren):_*)
-   }
-   case <head>{ ch @ _* }</head> => {
-   ch foreach(c => {
-   (headChildren find (sameAs(c, _))) match {
-   case None => headChildren &+ c
-   case _ =>
-   }
-   })
-   Nil
-   }
-   case Elem(namespace, label, attrs, scp, ns @ _*) => Elem(namespace, label, attrs, scp, extractHead2(ns):_* )
-   case Group(nodes) => nodes.flatMap(extractHead)
-   case _ if (node.isInstanceOf[Node]) => node
-   case _ => node.flatMap(extractHead2)
-   }
-   extractHead2(xhtml)
-   }
-
-   def cleanHead(htmlHead: NodeSeq) : NodeSeq = {
-   val newHead = new NodeBuffer()
-   htmlHead.foreach(node => if (!newHead.exists(sameAs(node, _))) newHead &+ node)
-   newHead
-   }
-
-   def sameAs(node1: Node, node2: Node): Boolean = {
-   if (node1.label.equals(node2.label)) {
-   node1.label match {
-   case "title" => true
-   case label if (label == "style" || label == "script" || label == "link") => {
-   (!node1.attribute("id").isEmpty && node1.attribute("id").equals(node2.attribute("id"))) ||
-   (!node1.attribute("src").isEmpty && node1.attribute("src").equals(node2.attribute("src"))) ||
-   (!node1.attribute("href").isEmpty && node1.attribute("href").equals(node2.attribute("href"))) ||
-   (!node1.child.isEmpty && (node1.child.equals(node2.child)))
-   }
-   case _ => false
-   }
-   } else {
-   false
-   }
-   }*/
 
 }
 
