1f9ddc34b46e4a51313f4648a5fc3d9015b3994a
Alex Boisvert <boisvert@intalio.com>
2009-03-26 16:08:27 -0700

    Need TestPaypal to produce a class

--- lift-paypal/src/test/scala/net/liftweb/paypal/TestPaypal.scala
+++ lift-paypal/src/test/scala/net/liftweb/paypal/TestPaypal.scala
@@ -8,10 +8,10 @@ import org.junit.Assert._
 import net.liftweb.paypal._
 
 
-//class TestPaypal {
+class TestPaypal {
   //@Test
   //def general: PaypalDataTransferReponse =
   //   PaypalDataTransfer("eEprQP3Xnm41ehWuDMZIeniqyWRyB2qOl2M6jKmbTQZJT8nBbey0ifT_fjG","5A431051B14324731").execute
-//}
+}
 
 
